7f4a5c7c1614beef7263c1cfcad94515c30a2baa
Harsh Guleria <guleriah59@gmail.com>
2025-11-09 08:20:55 +0530

    cleaning the react code and write hello world

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <h1>
-      Chai aur React with Vite | Learning how to set up React + Vite with Hitesh Sir
+      Chai aur React | Learning how to set up React + Vite with Hitesh Sir 
       understanding every line of code, not just copy-paste 🔥 | Harsh Guleria
     </h1>
   );
