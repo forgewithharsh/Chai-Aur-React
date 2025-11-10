58bb38d5ea603a0409491705471f3dc9207eab1a
Harsh Guleria <guleriah59@gmail.com>
2025-11-10 18:16:17 +0530

    refactor: clean react code

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,14 @@
+import Chai from "./Chai.jsx";
+
 function App() {
   return (
-    <h1>
-      Chai aur React | Learning how to set up React + Vite with Hitesh Sir 
-      understanding every line of code, not just copy-paste 🔥 | Harsh Guleria
-    </h1>
+    <>
+      <h1>
+        Chai aur React | Learning how to set up React + Vite with Hitesh Sir
+        understanding every line of code, not just copy-paste 🔥 | Harsh Guleria
+      </h1>
+      <Chai />
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,15 @@
+import Chai from "./chai.jsx";
+
 function App() {
   return (
-    <h1>
-      Chai aur React with Vite | Learning how to set up React + Vite with Hitesh Sir
-      understanding every line of code, not just copy-paste 🔥 | Harsh Guleria
-    </h1>
+    <>
+      <h1>
+        Chai aur React with Vite | Learning how to set up React + Vite with
+        Hitesh Sir understanding every line of code, not just copy-paste 🔥 |
+        Harsh Guleria
+      </h1>
+      <Chai />
+    </>
   );
 }
 
