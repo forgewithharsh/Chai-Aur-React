56c5be543dd20d323f9dd9339519bd372299b4a7
Harsh Guleria <guleriah59@gmail.com>
2025-11-16 08:36:48 +0530

    starting a project of password generator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = () => {
+    
+  }
+
   return (
     <>
       <h1 className="text-4xl text-center text-white">Password Generator</h1>
