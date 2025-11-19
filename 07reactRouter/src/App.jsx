fd10fda17e5a076678483f2c861c6a09de787b64
Harsh Guleria <guleriah59@gmail.com>
2025-11-20 00:51:17 +0530

    start: react router crash course

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-600 p-4">React router</h1>
+    </>
+  );
+}
+
+export default App;
