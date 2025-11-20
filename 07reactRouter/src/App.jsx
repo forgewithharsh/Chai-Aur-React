c33dcefb8ed1e1c7edf28402f6443aaede1dbd1d
Harsh Guleria <guleriah59@gmail.com>
2025-11-21 00:08:41 +0530

    feat: added initial React Router setup with Outlet

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
-      <h1 className="bg-green-600 p-4">React router</h1>
     </>
   );
 }
