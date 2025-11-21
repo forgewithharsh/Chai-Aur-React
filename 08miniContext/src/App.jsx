5e3bcb96805a587d489c449b1141192ce2ff7249
Harsh Guleria <guleriah59@gmail.com>
2025-11-21 19:03:05 +0530

    start exploring context api

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1>React with Chai and share is important</h1>
+    </>
+  );
+}
+
+export default App;
