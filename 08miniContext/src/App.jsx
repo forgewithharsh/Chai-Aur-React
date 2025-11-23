bb21bd63c700cf137995287494fea501976b5d1f
Harsh Guleria <guleriah59@gmail.com>
2025-11-23 17:06:37 +0530

    context api project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import UserContext from "./context/UserContext";
 import UserContextProvider from "./context/UserContextProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <UserContextProvider>
       <h1>React with Chai and share is important</h1>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   );
 }
